35ce04ab3f2531572281e2fb985bb32afb231a86
TomaszMarcinkowski86 <9454031marcinkowski@gmail.com>
2019-06-22 13:35:59 +0200

    statement();         prepareStatement();         callableStatement();         prepareStatement2();

--- src/main/java/pl/sda/jdbcjpa/JdbcMain.java
+++ src/main/java/pl/sda/jdbcjpa/JdbcMain.java
@@ -6,6 +6,73 @@ import java.sql.*;
 public class JdbcMain {
     public static void main(String[] args) {
         statement();
+        prepareStatement();
+        callableStatement();
+        prepareStatement2();
+    }
+
+    private static void prepareStatement2() {
+        try(Connection connection = getConnection()){
+            int minSalary=1000;
+            String query = "select ename, job, sal, mgr " +
+                    "from sdajdbc.employee " +
+                    "where sal > ?"; // znak zapytania bezpieczny bo jak by było sal >" minSalary to ktoś może coś dopisać np. drop DB
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1,minSalary);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                String ename = resultSet.getString("ename");
+                String job = resultSet.getString("job");
+                int sal = resultSet.getInt("sal");
+                Integer mgr = resultSet.getInt("mgr");
+                if(resultSet.wasNull()){
+                    mgr=null;
+                }
+                System.out.println(ename + " " + job + " " + sal+" "+mgr);
+            }
+            System.out.println();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void callableStatement() {
+        try(Connection connection = getConnection()){
+            String query="{call sdajdbc.getName(?,?)}";
+            CallableStatement callableStatement=connection.prepareCall((query));
+            callableStatement.setInt(1,7654);
+            callableStatement.registerOutParameter(2,Types.VARCHAR);
+
+            callableStatement.execute();
+            String name = callableStatement.getString(2);
+            System.out.println(name);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+    }
+
+    private static void prepareStatement() {
+        try(Connection connection = getConnection()){
+            int minSalary=1000;
+            String query = "select ename, job, sal " +
+                    "from sdajdbc.employee " +
+                    "where sal > ?"; // znak zapytania bezpieczny bo jak by było sal >" minSalary to ktoś może coś dopisać np. drop DB
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1,minSalary);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                String ename = resultSet.getString("ename");
+                String job = resultSet.getString("job");
+                int sal = resultSet.getInt("sal");
+                System.out.println(ename + " " + job + " " + sal);
+            }
+            System.out.println();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
@@ -22,11 +89,7 @@ public class JdbcMain {
                 int sal = resultSet.getInt("sal");
                 System.out.println(ename + " " + job + " " + sal);
             }
-
-            // można tutaj podać dowolone zapytanie,
-            // chociaz rozwiązanie wydaje się być przestarzałe (jdbc- podstawa działania JPA i Hybernate)
-
-
+            System.out.println();
 
         }catch (SQLException e) {
             e.printStackTrace();
